a82da919ef6e3c2482332ee5ff215b16f7b4303d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-11 09:44:54 +0530

    core-java

--- src/main/java/com/barclays/corejava/demo/App.java
+++ src/main/java/com/barclays/corejava/demo/App.java
@@ -1,10 +1,17 @@
 package com.barclays.corejava.demo;
 
+/**
+ * 
+ * @author Vaman Deshmukh
+ *
+ */
+
 public class App {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hello world!");
+
 	}
 
 }
