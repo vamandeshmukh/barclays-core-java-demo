1333c7076867b50c813db710a2b0d308a613734d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-11 16:45:35 +0530

    design patterns

--- src/main/java/com/barclays/corejava/demo/dp/DesignPatternsDemo.java
+++ src/main/java/com/barclays/corejava/demo/dp/DesignPatternsDemo.java
@@ -4,11 +4,17 @@ package com.barclays.corejava.demo.dp;
 
 // types  - 
 
-// creations design patterns - create objects 
+// Creations design patterns - create objects 
 
-// behavioural design patterns 
+// Behavioral design patterns 
 
-// structural design patterns 
+// Structural design patterns 
+
+// wiki links: 
+// design patterns - https://en.wikipedia.org/wiki/Software_design_pattern
+//creations design patterns -  https://en.wikipedia.org/wiki/Creational_pattern 
+//behavioral design patterns - https://en.wikipedia.org/wiki/Behavioral_pattern
+//structural design patterns - https://en.wikipedia.org/wiki/Structural_pattern 
 
 public class DesignPatternsDemo {
 
