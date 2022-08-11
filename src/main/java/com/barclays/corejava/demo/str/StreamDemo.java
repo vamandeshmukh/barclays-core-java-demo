5c95a0d8bf8a3ab78d80a8005bfbf1dce4095775
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-11 11:41:42 +0530

    core-java

--- src/main/java/com/barclays/corejava/demo/str/StreamDemo.java
+++ src/main/java/com/barclays/corejava/demo/str/StreamDemo.java
@@ -2,6 +2,8 @@ package com.barclays.corejava.demo.str;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamDemo {
 
@@ -15,8 +17,24 @@ public class StreamDemo {
 		empList.add(new Employee(104, "Ponu", 80000));
 		empList.add(new Employee(105, "Gonu", 99000));
 
-		System.out.println("empList with forEach()");
+		System.out.println("All employees using forEach()");
 		empList.forEach(e -> System.out.println(e.toString()));
+
+//		System.out.println("Employees with salary > 90000");
+//		empList.forEach(e -> {
+//			if (e.getSalary() > 90000)
+//				System.out.println(e.toString());
+//		});
+
+		// fullform 
+		Stream<Employee> empStream = empList.stream();
+		List<Employee> firstThreeEmps = empStream.limit(3).collect(Collectors.toList());
+
+		// shortcut
+//		List<Employee> firstThreeEmps = empList.stream().limit(3).collect(Collectors.toList());
+
+		System.out.println("Only first three Employees");
+		firstThreeEmps.forEach(e -> System.out.println(e.toString()));
 
 	}
 
