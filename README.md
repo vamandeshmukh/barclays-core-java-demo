9a8433fccd8ed9186f70de5c82472263698408ca
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-11 09:26:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# barclays-core-java-demo
+ Core java demo project for Barclays trainees Aug 2022
---
+++ src/main/java/barclays/corejava/demo/App.java
@@ -0,0 +1,11 @@
+
+package barclays.corejava.demo;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+	}
+}
