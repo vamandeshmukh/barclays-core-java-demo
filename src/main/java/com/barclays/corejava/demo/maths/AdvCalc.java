cb1352ded15a947de3974c12c4f3d97dfc5fa575
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-12 16:02:22 +0530

    JUnit

---
+++ src/main/java/com/barclays/corejava/demo/maths/AdvCalc.java
@@ -0,0 +1,11 @@
+package com.barclays.corejava.demo.maths;
+
+public class AdvCalc {
+
+	public int addNumsWithGst(int i, int j, int k) {
+
+		return i + j + k;
+
+	}
+
+}
---
+++ src/test/java/com/barclays/corejava/demo/maths/AdvCalcTests.java
@@ -0,0 +1,23 @@
+package com.barclays.corejava.demo.maths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AdvCalcTests {
+
+	@Test
+	public void addNumsTest() {
+		AdvCalc ac = new AdvCalc();
+		assertEquals(60, ac.addNumsWithGst(10, 20, 30));
+
+	}
+
+	@Test
+	public void addNumsTest2() {
+		AdvCalc ac = new AdvCalc();
+		assertNotEquals(61, ac.addNumsWithGst(10, 20, 30));
+
+	}
+}
